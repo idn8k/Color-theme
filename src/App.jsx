1478dab464077a6db8f34913bc5d69aeab7aa24f
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-22 11:03:04 +0100

    found issue 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { initialColors } from "./lib/colors";
-import Color from "./Components/Color/Color";
-import "./App.css";
+import { initialColors } from './lib/colors';
+import Color from './Components/Color/Color';
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1>Theme Creator</h1>
+   return (
+      <>
+         <h1>Theme Creator</h1>
 
-      {initialColors.map((color) => {
-        return <Color key={color.id} color={color} />;
-      })}
-    </>
-  );
+         {initialColors.map((color) => {
+            return (
+               <Color
+                  key={color.id}
+                  color={color}
+               />
+            );
+         })}
+      </>
+   );
 }
 
 export default App;
+
+// Found the fixed issue!!
